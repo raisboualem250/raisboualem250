da70abd0c2e1274a246105eda63a591a9f63b434
kenzi <kenzi@kenzi.com>
2021-10-28 14:39:21 -0400

    path

---
+++ partie_02/PATH.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+path = Path.home() #to get the user folder
+print(path)
+path = Path.cwd() #ti get curent  worked directory
+print(path)
+path = Path.is_dir(path) #is directoty
+print(path)
+#.....
