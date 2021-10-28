1fba2e98c2216a20544f2c1f601f65c06696ded6
kenzi <kenzi@kenzi.com>
2021-10-28 15:02:05 -0400

    path

--- partie_02/PATH.py
+++ partie_02/PATH.py
@@ -1,8 +1,17 @@
 from pathlib import Path
 path = Path.home() #to get the user folder
 print(path)
-path = Path.cwd() #ti get curent  worked directory
-print(path)
+path1 = Path.cwd() #ti get curent  worked directory
+print(path1)
 path = Path.is_dir(path) #is directoty
 print(path)
+
 #.....
+path1 = Path.cwd() #to get curent  worked directory
+print(path1)
+path = path1 / "f0" #concatenate two path with /
+print(path)
+path = path.joinpath("f1","f2","qqq.txt") # concatenate using joinpath
+print(path)
+ext = path.suffix #get suffixe (extension)
+print(ext)
