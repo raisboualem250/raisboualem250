6ee4987e7526ad205a5a56b3bf81b3cb77ef9fd2
kenzi <kenzi@kenzi.com>
2021-10-29 09:11:04 -0400

    creat file

--- partie_02/PATH.py
+++ partie_02/PATH.py
@@ -18,6 +18,9 @@ ext = path.suffix #get suffixe (extension)
 print(ext)
 
 path2 = path2 / "test folder" 
-path2.mkdir(exist_ok = True) # creat a new test folder insode f0 mkdir(exist_ok = true)==> check if the folder exist it will not dcreat it and will not return error
+#path2.mkdir(exist_ok = True) # creat a new test folder insode f0 mkdir(exist_ok = true)==> check if the folder exist it will not dcreat it and will not return error
 path2 = path2 / "1" / "2" / "3"
-path2.mkdir(parents = True) # creat the folder and the uri 
+#path2.mkdir(parents = True) # creat the folder and the uri 
+
+path2 = path2 / "readme.txt" #create readme.txt path
+path2.touch() #creat file readme.txt
